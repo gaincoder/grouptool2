ce20943729dba53ade1ee1b1d18c8b4192dd541f
Gaincoder <gaincoder+git@gmail.com>
2019-09-14 16:22:21 +0200

    Change Password

--- src/Controller/SettingsController.php
+++ src/Controller/SettingsController.php
@@ -3,29 +3,71 @@
 namespace App\Controller;
 
 use App\Form\SettingsFormType;
+use App\Manager\GroupManagerInterface;
+use FOS\UserBundle\Model\UserManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Templating\EngineInterface;
 
-class SettingsController extends AbstractController
+class SettingsController
 {
+
+    /**
+     * @var EngineInterface
+     */
+    private $twig;
+    /**
+     * @var FormFactoryInterface
+     */
+    private $formFactory;
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+    /**
+     * @var UserManagerInterface
+     */
+    private $manager;
+    /**
+     * @var Security
+     */
+    private $security;
+    /**
+     * @var FlashBagInterface
+     */
+    private $flashBag;
+
+    public function __construct(EngineInterface $twig, FormFactoryInterface $formFactory, RouterInterface $router, Security $security, UserManagerInterface $manager,FlashBagInterface $flashBag )
+    {
+
+
+        $this->twig = $twig;
+        $this->formFactory = $formFactory;
+        $this->router = $router;
+        $this->manager = $manager;
+        $this->security = $security;
+        $this->flashBag = $flashBag;
+    }
+
     /**
      * @Route("/settings", name="settings")
      */
     public function indexAction(Request $request)
     {
-        $user = $this->getUser();
-        $form = $this->createForm(SettingsFormType::class, $user);
+        $user = $this->security->getUser();
+        $form = $this->formFactory->create(SettingsFormType::class, $user);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $user->telegramUsername = str_replace('@', '', $user->telegramUsername);
-            $em->persist($user);
-            $em->flush();
-
-            $this->addFlash('success', 'Einstellungen wurden gespeichert!');
+            $this->manager->updateUser($user);
+            $this->flashBag->add('success', 'Einstellungen wurden gespeichert!');
         }
-        return $this->render('closed_area/settings.html.twig', ['form' => $form->createView()]);
+        return new Response($this->twig->render('closed_area/settings.html.twig', ['form' => $form->createView()]));
     }
 
 
--- src/Form/SettingsFormType.php
+++ src/Form/SettingsFormType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use EmailBundle\Enums\Mails;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class SettingsFormType extends AbstractType
 {
@@ -12,6 +14,13 @@ class SettingsFormType extends AbstractType
     public function buildForm(\Symfony\Component\Form\FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('plainPassword', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'required' => false,
+                'first_options' => array('label' => 'Neues Passwort'),
+                'second_options' => array('label' => 'Passwort bestätigen'),
+                'invalid_message' => 'fos_user.password.mismatch',
+            ))
             ->add('mails',ChoiceType::class,[
                 'label' => 'Sende mir eine E-Mail wenn...',
                 'expanded'=>true,
